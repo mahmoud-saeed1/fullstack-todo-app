6eca851b2bc2c1abefc448df25fedb801bef3e54
mahmoud-saeed1 <hodaelgentle@gmail.com>
2024-08-16 22:21:21 +0300

    add fetch todos functionality using React query

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../config/axios.config";
 
 const Todos = () => {
-  return (
-    <div>Todos</div>
-  )
-}
+  /*~~~~~~~~$ Get JWT Key From Local Storage $~~~~~~~~*/
+  const storageKey = "loggedInUser";
+  const userDataString = localStorage.getItem(storageKey);
+  const userData = userDataString ? JSON.parse(userDataString) : null;
 
-export default Todos
+  /*~~~~~~~~$ React Query $~~~~~~~~*/
+  const { isPending, error, data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get("users/me?populate=todos", {
+        headers: { Authorization: `Bearer ${userData.jwt}` },
+      });
+      return data;
+    },
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      {data.todos.length ? (
+        data.todos.map((todo) => <h1 key={todo.id}>{todo.title}</h1>)
+      ) : (
+        <h1>no todos added</h1>
+      )}
+    </div>
+  );
+};
+
+export default Todos;
+
+//? this is the old way to fetch data using useEffect
+
+// import { useEffect, useState } from "react";
+// import axiosInstance from "../config/axios.config";
+
+// const Todos = () => {
+//   /*~~~~~~~~$ States $~~~~~~~~*/
+//   const [todos, setTodos] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   //? Get user data from local storage when component mounts and update it when user data changes
+//   const storageKey = "loggedInUser";
+//   const userDataString = localStorage.getItem(storageKey);
+//   const userData = userDataString ? JSON.parse(userDataString) : null;
+
+//   useEffect(() => {
+//     try {
+//       axiosInstance
+//         .get("users/me?populate=todos", {
+//           headers: { Authorization: `Bearer ${userData.jwt}` },
+//         })
+//         .then((res) => setTodos(res.data.todos))
+//         .catch((err) => console.log(err));
+//     } catch (error) {
+//       console.log(error);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   }, [userData]);
+
+//   if (isLoading) {
+//     return <h1>Loading...</h1>;
+//   }
+
+//   return (
+//     <div>
+//       {todos.length ? (
+//         todos.map((todo) => <h2 key={todo.id}>{todo.title}</h2>)
+//       ) : (
+//         <h1>no todos exist</h1>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Todos;
